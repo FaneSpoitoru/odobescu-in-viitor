6f1f214c74fbd1d6551f6f3d5a7ba4ed69e1c633
antidate723 <lapinfoo@gmail.com>
2025-10-16 16:42:55 +0300

    o prositie

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import WelcomePage from './pages/welcome_page'; 
+import Pareri from "./componente/Pareri";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<WelcomePage />} />
+      <Route path="/pareri" element={<Pareri />} />
     </Routes>
   );
 }
---
+++ src/componente/pareri.jsx
@@ -0,0 +1,14 @@
+import '../components_css/welcomeComp.css';
+
+
+function Welcome() {
+  return (
+    <div className="hero">
+      <h1>Pareri</h1>
+      <p>sa💡</p>
+      <a href="https://cnodobescu.ro/"><button>Descoperă mai mult despre liceu</button></a>
+    </div>
+  );
+}
+
+export default Welcome;
