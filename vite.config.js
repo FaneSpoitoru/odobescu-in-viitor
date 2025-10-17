565a69af783ebfbadf4462cf844f81e3223d4e7e
FaneSpoitoru <stefanologeanu2009@yahoo.com>
2025-10-17 18:32:37 +0300

    ss

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/odoebscu-in-viitor', // folosește căi relative
+  base: '/odobescu-in-viitor', // folosește căi relative
 });
